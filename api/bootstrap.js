1cd6bd35c112f07384ceb21c56cd3f8b1aad85c5
voidcakebot <voidcakebot@users.noreply.github.com>
2026-03-04 08:51:30 +0100

    Include daily completion status in bootstrap API

--- api/bootstrap.js
+++ api/bootstrap.js
@@ -1,7 +1,20 @@
 const { storage, ensureInit } = require('./_shared');
+const { berlinKeyNow } = require('../wiki');
 
 module.exports = async function(req,res){
   if (req.method !== 'GET') { res.statusCode=405; return res.json({ok:false,error:'Method not allowed'}); }
   await ensureInit();
-  return res.json({ ok:true, ...(await storage.bootstrap()) });
+
+  const base = await storage.bootstrap();
+  const todayKey = berlinKeyNow();
+  const completionByCharacter = {};
+
+  await Promise.all(
+    (base.taken || []).map(async (character) => {
+      const progress = await storage.getProgress(character, todayKey);
+      completionByCharacter[character] = Number(progress?.answeredCount || 0) >= 3;
+    })
+  );
+
+  return res.json({ ok:true, ...base, todayKey, completionByCharacter });
 };
